b4ffc5811b0c19bf430372e03a3c45a6ed2b24d7
Arthur <Arthur@MYPOWERMYBRAINM>
2021-07-27 04:24:37 +0400

    Code REWRITED from zero ;) Some optimization and delete trash

--- PGC/Functions.cs
+++ PGC/Functions.cs
@@ -1,16 +1,13 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Text.RegularExpressions;
-using System.IO;
-using System.Threading;
 using System.Diagnostics;
-using System.Windows.Forms;
-using Microsoft.Win32;
+using System.IO;
+using System.Linq;
 using System.Net;
-using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows.Forms;
 using xNet;
 
 namespace PGC
